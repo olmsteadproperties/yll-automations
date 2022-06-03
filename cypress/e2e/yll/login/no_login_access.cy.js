f69dbff53dd3ba30dce5a6b4e9f6162ae20962dd
Michael Katic <michaelkatic@gmail.com>
2022-06-02 20:43:02 -0700

    Cleaned up first test.

--- cypress/e2e/yll/login/no_login_access.cy.js
+++ cypress/e2e/yll/login/no_login_access.cy.js
@@ -1,13 +1,17 @@
 /// <reference types="cypress" />
 
 describe('example to-do app', () => {
+    const signInTextClass = '.MuiTypography-root.MuiTypography-h3.MuiTypography-paragraph.css-zoxu81';
+    const expectedSignInText = 'Sorry, you must be logged in to view this content!';
+
     beforeEach(() => {
+        cy.viewport(1024, 768)
         cy.visit('https://dev.app.yourlandloans.com/loans')
     })
 
     it('Should restrict access if you have not logged in yet.', () => {
-        cy.get('.MuiTypography-root.MuiTypography-h3.MuiTypography-paragraph.css-zoxu81').should('have.length', 1)
-        cy.get('.MuiTypography-root.MuiTypography-h3.MuiTypography-paragraph.css-zoxu81').should('have.text', 'Sorry, you must be logged in to view this content!');
+        cy.get(signInTextClass).should('have.length', 1)
+        cy.get(signInTextClass).should('have.text', expectedSignInText);
     })
 })
 
