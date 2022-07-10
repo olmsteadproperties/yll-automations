7fc6655f4728334c41d265e198132ec1894842f3
mkatic <michaelkatic@gmail.com>
2022-07-10 11:15:43 -0700

    Added private variables to env and other cypress configuration updates.

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,24 @@
 const { defineConfig } = require("cypress");
 
+// Populate process.env with values from .env file
+require('dotenv').config()
+
 module.exports = defineConfig({
+  viewportWidth: 2048,
+  viewportHeight: 1024,
+
+  watchForFileChanges: false,
+
+  chromeWebSecurity: false,
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    env: {
+      googleEmail: process.env.GOOGLE_EMAIL,
+      googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      googleClientId: process.env.GOOGLE_CLIENTID,
+      googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      appEmailSender: 'no-reply@yourlandloans.com'
+    }
   },
 
   component: {
@@ -14,3 +28,13 @@ module.exports = defineConfig({
     },
   },
 });
+
+
+// async function getEmail(){
+//     let inbox = new Inbox('credentials_gmail.json');
+//     await inbox.authenticateAccount(); // logs user in
+    
+//     let messages = await inbox.getLatestMessages();
+
+//     console.log("my inbox messages", JSON.stringify(messages,null,4));
+// }
