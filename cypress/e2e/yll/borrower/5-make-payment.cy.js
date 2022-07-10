fc846d2f0b646cadfc5439679e3c9fd89163f71a
mkatic <michaelkatic@gmail.com>
2022-07-10 11:22:34 -0700

    Added borrower signup -> pay flow. All test currently passing.

---
+++ cypress/e2e/yll/borrower/5-make-payment.cy.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+import accounts from "../../../support/yll/accounts";
+import selectors from "../../../support/yll/selectors";
+import paths from "../../../support/yll/paths";
+import {login, navigate, randomString} from "../../../support/yll/util";
+import {generatedAccounts} from '../../../support/output/generatedAccounts.json';
+import 'cypress-wait-until';
+
+const lastAccountAddedKey = Object.keys(generatedAccounts).slice(-1);
+const lastAccountAdded = generatedAccounts[lastAccountAddedKey];
+const lastBankAccountAddedKey = Object.keys(lastAccountAdded.bankAccounts).slice(-1);
+const lastBankAccountAdded = lastAccountAdded.bankAccounts[lastBankAccountAddedKey];
+
+const loanName = "Cypress Test Loan"
+
+describe('Add Borrower to Loan', () => {
+    before(() => {
+        login({account: lastAccountAdded});
+    })
+
+    it('Make Payment on Loan', () => {
+        navigate(paths.loansMakePayment);
+
+        cy.get('div.MuiSelect-select').click();
+        cy.contains('p', loanName).click();
+
+        cy.get('input#paymentAmount').type("1234.56");
+
+        var regex1 = /[^0-9](?=[0-9])/g; 
+        var regex2 = /[^a-zA-Z](?=[a-zA-Z])/g;  
+        var nameWithSpaces = lastBankAccountAdded.bankName.replace(regex1, '$& ').replace(regex2, '$& ');
+        cy.log('name with spaced ' + nameWithSpaces);
+        cy.contains('Select Bank Account').parent().contains(nameWithSpaces, {matchCase: false}).click();
+        cy.contains('Payment Timing').parent().contains('Payment Due Now').click();
+        cy.contains('label', 'Notes (Optional)').parent().get('input[placeholder="Extra January Payment"]').type('Cypress automated test payment.');
+
+        cy.contains('button', 'Confirm Payment').click();
+
+        // cy.contains('button', 'Confirm Payment of $1,234.56').click()
+
+        cy.get('form').submit() // Have to use direct form submit instead. 
+    })
+})
+
