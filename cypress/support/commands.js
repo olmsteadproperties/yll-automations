922be5732ae9727b0efee9bc68d18fccadc47b67
mkatic <michaelkatic@gmail.com>
2022-07-10 11:17:59 -0700

    Added custom "embedded" command for getting content from iframe or shadow dom.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,8 +7,9 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
+
+import 'cypress-iframe';
+
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
@@ -23,3 +24,59 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//https://www.cypress.io/blog/2020/02/12/working-with-iframes-in-cypress/
+Cypress.Commands.add('embeded', (isIframe, method, params) => {
+    if (isIframe)
+    {
+        if (method === "get")
+        {
+            method = "find";
+        }
+        return cy.iframe()[method](...params);
+    }
+    else
+    {
+        params.push({ includeShadowDom: true });
+        return cy[method](...params);
+    }
+
+});
+
+
+
+// Cypress.Commands.add('getEmailInbox', () => {
+//     cy.log('Logging in to Google')
+//     cy.request({
+//       method: 'POST',
+//       url: 'https://www.googleapis.com/oauth2/v4/token',
+//       body: {
+//         grant_type: 'refresh_token',
+//         client_id: Cypress.env('googleClientId'),
+//         client_secret: Cypress.env('googleClientSecret'),
+//         refresh_token: Cypress.env('googleRefreshToken'),
+//       },
+//     }).then(({ body }) => {
+//       const { access_token, id_token } = body
+  
+//       cy.request({
+//         method: 'GET',
+//         url: 'https://gmail.googleapis.com/gmail/v1/users/' + Cypress.env('googleEmail') + '/messages',
+//         headers: { Authorization: `Bearer ${access_token}` },
+//       }).then(({ body }) => {
+//         cy.log(body)
+//         const userItem = {
+//           token: id_token,
+//           user: {
+//             googleId: body.sub,
+//             email: body.email,
+//             givenName: body.given_name,
+//             familyName: body.family_name,
+//             imageUrl: body.picture,
+//           },
+//         }
+  
+//         window.localStorage.setItem('googleCypress', JSON.stringify(userItem))
+//       })
+//     })
+//   })
