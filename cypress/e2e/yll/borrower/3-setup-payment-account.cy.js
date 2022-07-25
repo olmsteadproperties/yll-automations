a1f73b51226ebbd0403619c8e2ba13a02b2439a1
mkatic <michaelkatic@gmail.com>
2022-07-24 20:40:15 -0700

    Updated to support new bank account verification UI. All tests back to green.

--- cypress/e2e/yll/borrower/3-setup-payment-account.cy.js
+++ cypress/e2e/yll/borrower/3-setup-payment-account.cy.js
@@ -19,6 +19,11 @@ const bankAccounts = {
         routingNumber: "072403004",
         accountNumber: "856666",
         description: "ACH/eCheck bank account number that is rejected immediately, at the time of the transaction"
+    },
+    dwalla: {
+        routingNumber: "222222226", //https://developers.dwolla.com/guides/sandbox#test-bank-account-numbers
+        accountNumber: "856666",
+        description: "Dwalla specific test account"
     }
 }
 
@@ -61,71 +66,35 @@ describe('Set Up Borrower Payment Method', () => {
                 cy.embeded(false, 'get', ['input#dwolla-customer-create-submit[value="Agree and Continue"]']).click();
             }
 
-            //https://www.cypress.io/blog/2020/02/12/working-with-iframes-in-cypress/
-            // const iframeRefresh = () => {
-            //     cy.wait(3000);
-            //     cy.frameLoaded('iframe');
-            // }
-
-            // //Add Payment Method
-            // cy.contains('span', 'Add Payment Method'); //Can take a second to load this page so wait to see the header.
-
-            // iframeRefresh();
-            // cy.embeded(true, 'contains', ['h4', 'Search for your bank or credit union.']);  //Also takes a second for Dwalla to load so wait until you can see this header too. 
-            // cy.embeded(true, 'contains', ['a', 'Bank of America']).click({force: true});
-
-            // iframeRefresh();
-
-            // //Select verification type
-            // cy.embeded(true, 'contains', ['a', 'Use deposit verification']).click();
-
-            // iframeRefresh();
+            cy.contains('Micro Deposit Verification').parent().click()
 
             //Microdeposit validation //TODO: test Instant Account Verification (IAV)
-            const microDepositForm = cy.contains('Using Micro Deposit').parent(); 
+            const microDepositForm = () => cy.contains('Please fill bank details below').parent(); 
+
+            //Add bank account information
+            const newBankAccount = copyObject(bankAccounts.dwalla)
+            newBankAccount.bankName = randomString();
+            newBankAccount.verified = false;    
             
-            microDepositForm.within(() => {
-                //Add bank account information
-                const newBankAccount = copyObject(bankAccounts.success)
-                newBankAccount.bankName = randomString();
-                newBankAccount.verified = false;    
+            microDepositForm().within(() => {
                 cy.get('input[name="routingNumber"]').type(newBankAccount.routingNumber);
                 cy.get('input[name="accountNumber"]').type(newBankAccount.accountNumber);
                 cy.get('input[name="bankName"]').type(newBankAccount.bankName);
                 cy.contains('Account Type').parent().click();
             })
 
-            cy.contains('.MuiButtonBase-root', 'Savings').click()
+            cy.contains('.MuiButtonBase-root', 'Savings').click();
 
-            //Cannot click thie button? "Element is being covered by another element"
-            // microDepositForm.within(() => { 
-            //     cy.get('button[type="submit"]').click();
-            //     cy.contains('button', 'Add Bank').click();
-            // })
+            cy.contains('Add Bank').click();
 
-            const microDepositFormSubmit = cy.contains('Using Micro Deposit').parent(); //Somehow the value of microDepositForm is changing so we have to get it again.
-            microDepositFormSubmit.submit(); // Have to use direct form submit instead. 
-
-
-
-            //TODO: fix new UI stuff
-            
-            // //Add bank account information
-            // const newBankAccount = copyObject(bankAccounts.success)
-            // newBankAccount.bankName = randomString();
-            // newBankAccount.verified = false;
-            // cy.embeded(true, 'get', ['input#RoutingNumber']).type(newBankAccount.routingNumber);
-            // cy.embeded(true, 'get', ['input#AccountNumber']).type(newBankAccount.accountNumber);
-            // cy.embeded(true, 'get', ['input#BankAccountName']).type(newBankAccount.bankName);
-            // cy.embeded(true, 'contains', ['button', 'Agree & Continue']).click();
-
-            // const updatedAccount = copyObject(lastAccountAdded);
-            // let updateBankAccounts = typeof(updatedAccount.bankAccounts) == "undefined" ? {} : updatedAccount.bankAccounts;
-            // updateBankAccounts[newBankAccount.bankName] = newBankAccount;
-            // updatedAccount.bankAccounts = updateBankAccounts;
-            // updatedAccount.dateUpdated = new Date().toString();
-            // generatedAccounts[lastAccountAdded.email] = updatedAccount;
-            // cy.writeFile('cypress/support/output/generatedAccounts.json', {generatedAccounts});
+            //Update generatedAccounts.json
+            const updatedAccount = copyObject(lastAccountAdded);
+            let updateBankAccounts = typeof(updatedAccount.bankAccounts) == "undefined" ? {} : updatedAccount.bankAccounts;
+            updateBankAccounts[newBankAccount.bankName] = newBankAccount;
+            updatedAccount.bankAccounts = updateBankAccounts;
+            updatedAccount.dateUpdated = new Date().toString();
+            generatedAccounts[lastAccountAdded.email] = updatedAccount;
+            cy.writeFile('cypress/support/output/generatedAccounts.json', {generatedAccounts});
         });
     });
 });
