c18bd9eed806142f54bb4f101d953a9b956a7399
mkatic <michaelkatic@gmail.com>
2022-07-27 20:33:48 -0700

    Added electron browser. Turning off video changed behavior of tests. Attempting to turn off video only on test success.

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   viewportHeight: 1024,
   watchForFileChanges: false,
   chromeWebSecurity: false,
-  video: false,
+  processVideoOnPassingTest: false,
 
   e2e: {
     env: {
